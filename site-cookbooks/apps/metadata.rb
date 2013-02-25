fbfcf82dea53c494efa9f3c8368a36354bdcc415
Dann <techmemo@gmail.com>
2013-02-25 22:14:31 +0900

    Add author to metadata

--- site-cookbooks/apps/metadata.rb
+++ site-cookbooks/apps/metadata.rb
@@ -1,7 +1,8 @@
 name             'macapp'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'dann'
+maintainer_email 'techmemo {at} gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures macapp'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+depends          'dmg'
--- site-cookbooks/brew/metadata.rb
+++ site-cookbooks/brew/metadata.rb
@@ -1,7 +1,8 @@
 name             'brew'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'dann'
+maintainer_email 'techmemo {at} gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures brewapp'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+depends          'homebrew'
