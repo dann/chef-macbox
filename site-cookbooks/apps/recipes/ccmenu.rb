7f92e9387c910a55b357c969a68bc188c6780b5d
Dann <techmemo@gmail.com>
2013-02-25 20:48:59 +0900

    Add ccmenu recipe

---
+++ site-cookbooks/apps/recipes/ccmenu.rb
@@ -0,0 +1,9 @@
+include_recipe "dmg"
+
+dmg_package "CCMenu" do
+  volumes_dir "CCMenu 1.3"
+  source "http://surfnet.dl.sourceforge.net/project/ccmenu/CCMenu/1.3/ccmenu-1.3-b.dmg"
+  checksum "9afef887022002b0f074eb7db25349da53aea5d5a64d8415b756c676db9ad1de"
+  action :install
+end
+
